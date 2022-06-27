1a03db2651766689ef8063ed975959dd2d9dc99a
MariaLuizaSantana <costamalu123@outlook.com>
2022-06-27 16:44:16 -0300

    adicionando a page de componentes

---
+++ src/Components/radio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Radio = (className, type, name, onChange, value) => (
+    <input 
+    type={type} 
+    value={value} 
+    name={name} 
+    onChange={onChange} 
+    className={className}
+    required/>
+);
+
+export default Radio;
