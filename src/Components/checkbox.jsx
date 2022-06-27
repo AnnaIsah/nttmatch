8bfde28cfd84c70c74b681ac6fe19747cc12d87f
MariaLuizaSantana <costamalu123@outlook.com>
2022-06-27 19:15:41 -0300

    finalizando o input checked

--- src/Components/checkbox.jsx
+++ src/Components/checkbox.jsx
@@ -4,10 +4,11 @@ const Checkbox = (className, name, onChange, checked) => (
     <input 
     type="checkbox"
     value={value} 
-    name={name} 
+    name={name}
+    checked={checked}
     onChange={onChange} 
     className={className}
-    required/>
+    />
 );
 
 export default Checkbox;
